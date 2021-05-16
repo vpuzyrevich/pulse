6c7f56b9a11578f3892b4842158523b5c533b540
vpuz <v.puzyrevich@gmail.com>
2021-05-16 17:51:21 +0300

    last commit

--- src/js/script.js
+++ src/js/script.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
             });
         });
-    }
+    };
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
@@ -69,11 +69,45 @@ $(document).ready(function(){
                 }
               }
         });
-    }
+    };
 
     validaForms('#consultation-form');
     validaForms('#consultation form');
     validaForms('#order form');
 
     $('input[name=phone]').mask("+375 (99) 999-99-99");
-  });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+    new WOW().init();
+});
